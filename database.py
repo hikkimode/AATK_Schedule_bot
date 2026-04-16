b8044f5015c7ce83d3206342b9e1aefaf743faba
StrongAidyn <csgosleepy63@gmail.com>
2026-04-17 03:35:15 +0500

    fix: total disable of statement caching for pgbouncer

--- database.py
+++ database.py
@@ -6,7 +6,15 @@ from models import Base
 
 
 def create_engine_and_sessionmaker(database_url: str) -> tuple[AsyncEngine, async_sessionmaker[AsyncSession]]:
-    engine = create_async_engine(database_url, connect_args={"prepared_statement_cache_size": 0}, pool_pre_ping=True)
+    engine = create_async_engine(
+        database_url,
+        connect_args={
+            "prepared_statement_cache_size": 0,
+            "statement_cache_size": 0
+        },
+        pool_pre_ping=True,
+        pool_recycle=300
+    )
     session_factory = async_sessionmaker(engine, expire_on_commit=False)
     return engine, session_factory
 
